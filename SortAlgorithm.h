f84f9f46032f0c0cf01c19ee6240578c503e9cf6
pecuyu <pecuyu@outlook.com>
2021-03-11 21:03:27 +0800

    bubble sort optimization : 遍历序列没有改变,说明排序完成,提前结束

--- SortAlgorithm.h
+++ SortAlgorithm.h
@@ -66,13 +66,20 @@ namespace SortAlgorithm {
         assert(array != nullptr);
         assert(length > 0);
 
+        bool sorted;
         for (int i = 0; i < length; ++i) { // 遍历数组
+            sorted = true;
             for (int j = 0; j < length - i - 1; ++j) {
                 // 0...length-i ,每次遍历后,array[length-i-1] 为当前序列最值
                 // 比较 array[j] 和 array[(j+1)]
                 if (compare(array[j], array[j + 1])) {
                     swap(array[j], array[j + 1]);
+                    sorted = false; // 有交换,说明可能没有排序完成
                 }
+            }
+
+            if (sorted) { // 遍历序列没有改变,说明排序完成
+                break;
             }
         }
     }
