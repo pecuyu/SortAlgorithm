63ce8f464828a5b915c20ad2352c9608820307f2
pecuyu <pecuyu@outlook.com>
2021-03-24 23:22:38 +0800

    add tester for unit test

---
+++ Tester.h
@@ -0,0 +1,96 @@
+//
+// Created by pecuyu on 2021/3/24.
+//
+
+#ifndef SORTALGORITHM_TESTER_H
+#define SORTALGORITHM_TESTER_H
+
+#include "SortAlgorithm.h"
+#include "SortTestUtils.h"
+#include "QuickSort.h"
+#include "BubbleSort.h"
+#include "SelectionSort.h"
+#include "InsertionSort.h"
+#include "MergeSort.h"
+
+
+class Tester{
+
+public:
+
+    static void testBubbleSort(int length){
+        int* arr = SortTestUtils::generateRandomArray(length, 0, 100);
+        SortTestUtils::calculateSort("bubbleSort", arr, length,
+                                     SortAlgorithm::BubbleSort::bubbleSort, SortAlgorithm::more);
+
+        delete[] arr;
+    }
+
+    static void testSelectionSort(int length){
+        int* arr = SortTestUtils::generateRandomArray(length, 0, length);
+        int *nearlyOrderedArray = SortTestUtils::generateNearlyOrderedArray(length, 10);
+
+        SortTestUtils::calculateSort("selectionSort", arr, length, SortAlgorithm::SelectionSort::selectionSort);
+        SortTestUtils::calculateSort("selectionSort", nearlyOrderedArray, length, SortAlgorithm::SelectionSort::selectionSort);
+
+        delete[] arr;
+        delete[] nearlyOrderedArray;
+    }
+
+    static void testInsertionSort(int length){
+        int* arr = SortTestUtils::generateRandomArray(length, 0, length);
+        int *nearlyOrderedArray = SortTestUtils::generateNearlyOrderedArray(length, 100);
+        int *nearlyOrderedArrayCopy = SortTestUtils::copyIntArray(nearlyOrderedArray, length);
+
+        SortTestUtils::calculateSort("insertionSort", nearlyOrderedArray, length,
+                                     SortAlgorithm::InsertionSort::insertionSort, SortAlgorithm::more);
+        SortTestUtils::calculateSort("insertionSortOptimize", nearlyOrderedArrayCopy,
+                                     length, SortAlgorithm::InsertionSort::insertionSortOptimize, SortAlgorithm::less);
+
+        delete[] arr;
+        delete[] nearlyOrderedArray;
+        delete[] nearlyOrderedArrayCopy;
+    }
+
+    static void testMergeSort(int length){
+        int *nearlyOrderedArray = SortTestUtils::generateNearlyOrderedArray(length, 100);
+        int *nearlyOrderedArrayCopy = SortTestUtils::copyIntArray(nearlyOrderedArray, length);
+
+        SortTestUtils::calculateSort("mergeSort", nearlyOrderedArray,
+                                     length, SortAlgorithm::MergeSort::mergeSort);
+        SortTestUtils::calculateSort("mergeSortWithForLoop", nearlyOrderedArrayCopy, length,
+                                     SortAlgorithm::MergeSort::mergeSortWithForLoop);
+
+        delete[] nearlyOrderedArray;
+        delete[] nearlyOrderedArrayCopy;
+    }
+
+
+    static void testQuickSort(int length){
+        // 对大量重复元素的数组排序
+        int* arr = SortTestUtils::generateRandomArray(length, 0, 100); // 产生大量重复元素
+        int *arrCopy = SortTestUtils::copyIntArray(arr, length);
+        int *arrCopy2 = SortTestUtils::copyIntArray(arr, length);
+
+        SortTestUtils::calculateSort("quickSort", arr, length, SortAlgorithm::QuickSort::quickSort);
+        SortTestUtils::calculateSort("quickSort2", arrCopy, length, SortAlgorithm::QuickSort::quickSort2);
+        SortTestUtils::calculateSort("quickSort3Ways", arrCopy2, length, SortAlgorithm::QuickSort::quickSort3Ways);
+
+        delete[] arr;
+        delete[] arrCopy;
+        delete[] arrCopy2;
+
+        std::cout<<std::endl;
+
+        // 对近乎有序的数组进行排序
+        int *nearlyOrderedArray = SortTestUtils::generateNearlyOrderedArray(length, 10);
+        int *nearlyOrderedArrayCopy = SortTestUtils::copyIntArray(nearlyOrderedArray, length);
+        int *nearlyOrderedArrayCopy2 = SortTestUtils::copyIntArray(nearlyOrderedArray, length);
+        SortTestUtils::calculateSort("quickSort", nearlyOrderedArray, length, SortAlgorithm::QuickSort::quickSort);
+        SortTestUtils::calculateSort("quickSort2", nearlyOrderedArrayCopy, length, SortAlgorithm::QuickSort::quickSort2);
+        SortTestUtils::calculateSort("quickSort3Ways", nearlyOrderedArrayCopy2, length, SortAlgorithm::QuickSort::quickSort3Ways);
+    }
+
+};
+
+#endif //SORTALGORITHM_TESTER_H
--- main.cpp
+++ main.cpp
@@ -1,41 +1,16 @@
 #include <iostream>
-#include "SortAlgorithm.h"
-#include "SortTestUtils.h"
-#include "BubbleSort.h"
-#include "MergeSort.h"
-#include "QuickSort.h"
+
+#include "Tester.h"
 
 
 int main() {
 
     int length = 1000000;
-    int* arr = SortTestUtils::generateRandomArray(length, 0, 10);
-    int *arrCopy = SortTestUtils::copyIntArray(arr, length);
-    //int *nearlyOrderedArray = SortTestUtils::generateNearlyOrderedArray(length, 1000);
-    //int *nearlyOrderedArrayCopy = SortTestUtils::copyIntArray(nearlyOrderedArray, length);
-    //SortAlgorithm::selectionSort(arr, length);
-    //SortTestUtils::calculateSort("selectionSort", nearlyOrderedArray, length, SortAlgorithm::selectionSort);
-    //SortTestUtils::printArray(nearlyOrderedArray, length);
-
-    //SortTestUtils::calculateSort("bubbleSort", arr, length, SortAlgorithm::bubbleSort, SortAlgorithm::more);
-    //SortTestUtils::calculateSort("insertionSort", arr, length, SortAlgorithm::insertionSort, SortAlgorithm::more);
-   // SortTestUtils::calculateSort("insertionSortOptimize", nearlyOrderedArrayCopy, length, SortAlgorithm::insertionSortOptimize, SortAlgorithm::less);
-
-    //SortTestUtils::calculateSort("mergeSort", nearlyOrderedArrayCopy, length, SortAlgorithm::mergeSort);
-
-    SortTestUtils::calculateSort("mergeSortWithForLoop", arr, length,
-                                 SortAlgorithm::MergeSort::mergeSortWithForLoop);
-
-   // SortTestUtils::calculateSort("quickSort", arr, length, SortAlgorithm::QuickSort::quickSort);
-    SortTestUtils::calculateSort("quickSort2", arrCopy, length, SortAlgorithm::QuickSort::quickSort2);
-
-   // SortTestUtils::calculateSort("quickSort", arrCopy, length, quickSort);
-
-   // SortTestUtils::printArray(arrCopy, length);
-    //delete[] arr;
-    //delete[] arrCopy;
-   // delete[]nearlyOrderedArray;
-   // delete[] nearlyOrderedArrayCopy;
+    //Tester::testBubbleSort(length);
+    //Tester::testSelectionSort(length);
+    //Tester::testInsertionSort(length);
+    //Tester::testMergeSort(length);
+    Tester::testQuickSort(length);
 
     return 0;
 }
